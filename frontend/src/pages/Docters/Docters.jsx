c66df9e9bead9b36b6c28bfcd9ddad9614f078bd
sathvika <alwalasathvika@gmail.com>
2024-07-10 18:34:22 +0530

    third commit

--- frontend/src/pages/Docters/Docters.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Docters = () => {
-  return (
-    <div>Docters</div>
-  )
-}
-
-export default Docters
--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -4,8 +4,8 @@ import Services from "../pages/Services";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Contact from "../pages/Contact";
-import Doctors from "../pages/Docters/Docters";
-import DocterDetails from '../pages/Docters/DoctorDetails';
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorDetails from '../pages/Doctors/DoctorDetails';
 
 import {Routes,Route} from 'react-router-dom'
 
@@ -14,8 +14,8 @@ const Routers = () => {
   return <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/home' element={<Home/>}/>
-    <Route path='/docters' element={<Doctors/>}/>
-    <Route path='/docters/:id' element={<DocterDetails/>}/>
+    <Route path='/doctors' element={<Doctors/>}/>
+    <Route path='/doctors/:id' element={<DoctorDetails/>}/>
     <Route path='/login' element={<Login/>}/>
     <Route path='/register' element={<Signup/>}/>
     <Route path='/contact' element={<Contact/>}/>
