ca598c0c37757f5f83bd17cda0c63bb8e737aa68
sathvika <alwalasathvika@gmail.com>
2024-07-28 15:07:39 +0530

    3 commit

--- backend/auth/verifyToken.js
+++ backend/auth/verifyToken.js
@@ -3,21 +3,50 @@ import Doctor from '../models/DoctorSchema.js';
 import User from '../models/UserSchema.js';
 
 export const authenticate = async (req, res, next) => {
-    const authToken = req.headers.authorization;
+  const authToken = req.headers.authorization;
 
-    if (!authToken || !authToken.startsWith('Bearer ')) {
-        return res.status(401).json({ success: false, message: 'No token, authorization denied' });
+  if (!authToken || !authToken.startsWith('Bearer ')) {
+    return res.status(401).json({ success: false, message: "No token, authorization denied" });
+  }
+
+  try {
+    const token = authToken.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.userId = decoded.id;
+    req.role = decoded.role;
+
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return res.status(401).json({ message: 'Token is expired' });
     }
-
-    try {
-        const token = authToken.split(' ')[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Ensure you have a JWT_SECRET in your environment variables
-
-        // Optionally, you can fetch the user from the database using the decoded data
-        req.user = decoded; // Assuming the token contains user information
-
-        next();
-    } catch (err) {
-        res.status(401).json({ success: false, message: 'Token is not valid' });
-    }
+    return res.status(401).json({ success: false, message: "Invalid token" });
+  }
 };
+
+export const restrict = roles =>async(req,res,next)=>{
+    const userId = req.userId;
+    let user;
+
+    try{
+      const patient = await User.findById(userId);
+      const doctor = await Doctor.findById(userId);
+
+      if (patient) {
+        user = patient;
+      } else if (doctor) {
+        user = doctor;
+      } else {
+        return res.status(401).json({ success: false, message: "User not found" });
+      }
+
+      if (!roles.includes(user.role)) {
+        return res.status(401).json({ success: false, message: "You're not authorized" });
+      }
+
+      next();
+    } catch (error) {
+      return res.status(500).json({ success: false, message: "Server error" });
+    }
+  };
